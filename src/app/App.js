60c19a5268dc9a0640cf1fc7f757c8e37de7a9fb
Ankit <2002ankit2002@gmail.com>
2024-02-13 22:41:27 +0530

    final commit

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,15 @@
 import "./app.css";
-// import Signup from "../components/signup/Signup";
-// import Header from "../components/header/Header";
-// import Footer from "../components/footer/Footer";
+import Signup from "../components/signup/Signup";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
 import Login from "../components/login/Login";
 
 function App() {
   return (
     <div className="App">
-      {/* <Header />
+      <Header />
       <Signup />
-      <Footer /> */}
+      <Footer />
       <Login />
     </div>
   );
